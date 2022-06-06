225180b8fb0a4c65883c4b964162a77fcf3c9a9a
ramezbenaribia <ramezbenaribia@gmail.com>
2022-06-06 01:04:32 +0100

    final push

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -9,11 +9,6 @@ import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
-  const owner = {
-    username: 'ramez.ben.aribia',
-    email: 'ramez.ben.aribia@gmail.com',
-  };
-  let dbOwner: OwnerDto;
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
@@ -21,16 +16,7 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
-    // dbOwner = await app
-    //   .get<OwnersService>(OwnersService)
-    //   .findOne({ where: { username: owner.username } });
 
-    dbOwner = {
-      id: '57603cd2-533c-4791-8adc-cf3ac1448b7d',
-      username: 'ramez.ben.aribia',
-      email: 'ramez.ben.aribia@gmail.com',
-
-    }
   });
 
   it('/ (GET)', () => {
@@ -52,67 +38,6 @@ describe('AppController (e2e)', () => {
       .expect({ customers: await app.get(CustomerService).getAllCustomers() });
   });
 
-
-  // it('/api/products/:id (GET)', async () => {
-  //   return request(app.getHttpServer())
-  //     .get('/api/products/fd097652-1cfa-4c98-bff8-d85efc43b007')
-  //     .expect(200)
-  //     .then((resp) => {
-  //       const payload = resp.body;
-  //       delete payload.owner.id;
-  //       expect({
-  //         id: 'fd097652-1cfa-4c98-bff8-d85efc43b007',
-  //         name: 'SPEAKER',
-  //         description: null,
-  //         product_type: 'IT',
-  //         owner: dbOwner,
-  //         customers: [],
-  //       });
-  //     });
-  // });
-  // it('/api/products (POST)', async () => {
-  //   return request(app.getHttpServer())
-  //     .post('/api/products')
-  //     .send({
-  //       name: 'test-product-name',
-  //       description: 'Random description',
-  //       product_type: 'product type',
-  //       userId: '57603cd2-533c-4791-8adc-cf3ac1448b7d',
-  //     })
-  //     .expect(201)
-  //     .then((response) => {
-  //       const payload = response.body;
-  //       delete payload.id;
-  //       expect(payload).toStrictEqual({
-  //         name: 'test-product-name',
-  //         description: 'Random description',
-  //         product_type: 'product type',
-  //         owner: dbOwner,
-  //       });
-  //     });
-  // });
-  // it('/api/products (PUT)', async () => {
-  //   return request(app.getHttpServer())
-  //     .put('/api/products/fd097652-1cfa-4c98-bff8-d85efc43b007')
-  //     .send({
-  //       name: 'Edited Name',
-  //       description: 'Edited description of the Office Chores',
-  //       product_type: 'updated product Type',
-  //       userId: dbOwner.id,
-  //     })
-  //     .expect(200)
-  //     .then((resp) => {
-  //       const payload = resp.body;
-  //       expect({
-  //         id: 'fd097652-1cfa-4c98-bff8-d85efc43b007',
-  //         name: 'Edited Name',
-  //         description: 'Edited description of the Office Chores',
-  //         product_type: 'updated product Type',
-  //         owner: dbOwner,
-  //         customers: [],
-  //       }).toStrictEqual(payload);
-  //     });
-  // });
   afterAll(async () => {
     await app.close();
   });
